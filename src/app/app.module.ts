2c4aa51e2e650cecca3c30e0e40b180cf9cd3872
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-05-06 05:23:56 -0700

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,14 @@ import {MatInputModule} from '@angular/material/input'
 import { MatNativeDateModule } from '@angular/material/core';
 import { PdfComponent } from './pdf/pdf.component';
 import { FormsModule } from '@angular/forms';
+import { ParentComponent } from './component/parent/parent.component';
+import { ChildComponent } from './component/child/child.component';
 @NgModule({
   declarations: [
     AppComponent,
-    PdfComponent
+    PdfComponent,
+    ParentComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
