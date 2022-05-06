5ed41441b59308ad5c7ab6e57ce5b31ffd0510ab
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-05-06 09:45:12 -0700

    Update child.component.ts

--- src/app/component/child/child.component.ts
+++ src/app/component/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit , Input } from '@angular/core';
+import { Component, OnInit , Input , Output , EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -8,10 +8,19 @@ import { Component, OnInit , Input } from '@angular/core';
 export class ChildComponent implements OnInit {
 @Input() recieved : string
 
+public toSend : string 
   constructor() { }
 
+@Output() sender = new EventEmitter();
+
+
+
   ngOnInit(): void {
-console.log(this.recieved)
+// console.log(this.recieved)
+this.toSend="hellow from parent";
+
+
+this.sender.emit(this.toSend);
   }
 
 }
