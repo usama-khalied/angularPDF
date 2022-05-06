f8c5b5790bf1c0b27208338dfd8a4341f610c285
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-05-06 09:45:22 -0700

    Update parent.component.ts

--- src/app/component/parent/parent.component.ts
+++ src/app/component/parent/parent.component.ts
@@ -6,20 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./parent.component.css']
 })
 export class ParentComponent implements OnInit {
-arr : any = [
-  {
-    id :1,
-    name :"Usama"
-  },
-  {
-    id :1,
-    name :"Usama"
-  },
-  {
-    id :1,
-    name :"Usama"
-  }
-]
+// arr : any = [
+//   {
+//     id :1,
+//     name :"Usama"
+//   },
+//   {
+//     id :1,
+//     name :"Usama"
+//   },
+//   {
+//     id :1,
+//     name :"Usama"
+//   }
+// ]
+// arr : string = "child to Parent" 
+
+
+
+reciever(event : string){
+  console.log(event)
+}
   constructor() { }
 
   ngOnInit(): void {
